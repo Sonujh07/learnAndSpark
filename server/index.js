903ba7d61338e037d5d746d90330c3f7dcf67fb3
Sonu Kumar <121501179+Sonujh07@users.noreply.github.com>
2026-05-11 08:56:32 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://learn-and-spark.vercel.app",
     credentials: true,
   })
 );
